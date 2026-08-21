45866f3a344b90c18ccf608686a2453fb74185a6
Yasmin1801 <yasmin.ok@icloud.com>
2026-08-21 02:43:47 +0000

    Exemplo2

---
+++ Exemplo1.java
@@ -0,0 +1,14 @@
+public class Exemplo1 {
+    public static void main (String [] args){
+
+        System.out.print("Esse algoritimo irá listar os meus 5 livros favoritos.");
+        System.out.println("Filme 1: Senhor dos aneis.");
+        System.out.println("Filme 2: Ponyo.");
+        System.out.println("Filme 3: Belle.");
+        System.out.println("Filme 4: Minions.");
+        System.out.println("Filme 5: Odisseia.");
+
+    
+    }
+
+}
---
+++ Exemplo2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Exemplo2{
+    public static void main (String[]args){
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Listar os 5 livros favoritos."); 
+
+        System.out.println("Digite qual é o seu livro favorito:");
+        String Livro1 = entrada.nextLine();
+
+        System.out.println("Digite qual é o seu segundo livro favorito:");
+        String Livro2 = entrada.nextLine();
+
+        System.out.println("Digite qual é o seu terceiro livro favorito:");
+        String Livro3 = entrada.nextLine();
+
+        System.out.println("Digite qual é o seu quarto livro favorito:");
+        String Livro4 = entrada.nextLine();
+
+        System.out.println("Digite qual é o seu quinto livro favorito:");
+        String Livro5 = entrada.nextLine();
+
+        System.out.println("\nSeus 5 livros favoritos são:");
+        System.out.println("1. "+Livro1);
+        System.out.println("2. "+Livro2);
+        System.out.println("3. "+Livro3);
+        System.out.println("4. "+Livro4);
+        System.out.println("5. "+Livro5);
+        entrada.close();
+    }
+    
+}
