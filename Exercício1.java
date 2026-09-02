c1b3bf672f9223c6265792323f8987e422aa0161
Yasmin1801 <yasmin.ok@icloud.com>
2026-09-02 11:05:46 -0300

    Add files via upload

---
+++ Exercício1.java
@@ -0,0 +1,8 @@
+public class Exercício1 {
+    public static void main(String[]args){
+        System.out.println("O meu nome completo é Yasmin Mayumi Okubo Sato.");
+        System.out.println("O meu registro acadêmico é " +6322084+ ".");
+        System.out.println("O meu curso é Análise e Desenvolvimento de Sistemas.");
+    }
+    
+}
